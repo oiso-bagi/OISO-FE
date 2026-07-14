c0d9fb607b49ad97f5f7656f3d95ff48433ade98
jeniferkim <jeniferkim1673@naver.com>
2026-07-15 01:58:10 +0900

    design: 타이포 fontFamily 일부 수정

--- src/shared/styles/typography.css.ts
+++ src/shared/styles/typography.css.ts
@@ -13,30 +13,29 @@
  */
 
 import { style } from "@vanilla-extract/css";
-
 import { vars } from "@/shared/styles/theme.css.ts";
 
 // =========================
-// Title
+// Title (Black Han Sans)
 // =========================
 
 export const largeTitleR32 = style({
-  fontFamily: vars.font.heading,
+  fontFamily: vars.font.heading, // Black Han Sans
   fontSize: "32px",
   fontWeight: vars.fontWeight.regular,
   lineHeight: "134%",
 });
 
 export const title1 = style({
-  fontFamily: vars.font.heading,
-  fontSize: "24px",
+  fontFamily: vars.font.heading, // Black Han Sans
+  fontSize: "24px", // Title1_R_24
   fontWeight: vars.fontWeight.regular,
   lineHeight: "134%",
 });
 
 export const title2 = style({
-  fontFamily: vars.font.heading,
-  fontSize: "22px",
+  fontFamily: vars.font.heading, // Black Han Sans
+  fontSize: "22px", // Title2_R_22
   fontWeight: vars.fontWeight.regular,
   lineHeight: "28px",
 });
@@ -46,28 +45,28 @@ export const title2 = style({
 // =========================
 
 export const largeBody1 = style({
-  fontFamily: vars.font.body,
+  fontFamily: vars.font.heading,
   fontSize: "32px",
   fontWeight: vars.fontWeight.regular,
   lineHeight: "134%",
 });
 
 export const largeBody2 = style({
-  fontFamily: vars.font.body,
+  fontFamily: vars.font.heading,
   fontSize: "18px",
   fontWeight: vars.fontWeight.regular,
   lineHeight: "132%",
 });
 
 export const largeBody3 = style({
-  fontFamily: vars.font.body,
+  fontFamily: vars.font.heading,
   fontSize: "16px",
   fontWeight: vars.fontWeight.regular,
   lineHeight: "28px",
 });
 
 // =========================
-// Body
+// Body (Pretendard)
 // =========================
 
 export const body1 = style({
@@ -81,21 +80,21 @@ export const body2 = style({
   fontFamily: vars.font.body,
   fontSize: "16px",
   fontWeight: vars.fontWeight.bold,
-  lineHeight: "134%",
+  lineHeight: "134%", // Body2_B_16
 });
 
 export const body3 = style({
   fontFamily: vars.font.body,
   fontSize: "16px",
   fontWeight: vars.fontWeight.medium,
-  lineHeight: "24px",
+  lineHeight: "24px", // Body3_M_16
 });
 
 export const body4 = style({
   fontFamily: vars.font.body,
   fontSize: "14px",
   fontWeight: vars.fontWeight.bold,
-  lineHeight: "20px",
+  lineHeight: "20px", // Body4_B_14
 });
 
 export const body5 = style({
@@ -134,33 +133,33 @@ export const body9 = style({
 });
 
 // =========================
-// Detail
+// Detail (Pretendard)
 // =========================
 
 export const detail1 = style({
   fontFamily: vars.font.body,
   fontSize: "14px",
   fontWeight: vars.fontWeight.medium,
-  lineHeight: "20px",
+  lineHeight: "20px", // Detail1_M_14
 });
 
 export const detail2 = style({
   fontFamily: vars.font.body,
   fontSize: "12px",
   fontWeight: vars.fontWeight.bold,
-  lineHeight: "16px",
+  lineHeight: "16px", // Detail2_B_12
 });
 
 export const detail3 = style({
   fontFamily: vars.font.body,
   fontSize: "10px",
   fontWeight: vars.fontWeight.bold,
-  lineHeight: "14px",
+  lineHeight: "14px", // Detail3_B_10
 });
 
 export const detail4 = style({
   fontFamily: vars.font.body,
   fontSize: "10px",
   fontWeight: vars.fontWeight.medium,
-  lineHeight: "14px",
+  lineHeight: "14px", // Detail4_M_10
 });
