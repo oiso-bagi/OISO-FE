ad4cb91f4b18b5be6414d4eab8afc5e03b3ad8c9
jeniferkim <jeniferkim1673@naver.com>
2026-07-15 01:33:14 +0900

    chore: 타이포 토큰 이름 수정

--- src/shared/styles/typography.css.ts
+++ src/shared/styles/typography.css.ts
@@ -2,7 +2,7 @@
  * Typography Design System
  *
  * 사용 예시)
- * import * as typo from "@/shared/styles/typography.css";
+ * import * as typo from "@/shared/styles/typography.css.ts";
  *
  * <h1 className={typo.title2R24}>제목</h1>
  * <p className={typo.body6M14}>본문</p>
@@ -14,7 +14,7 @@
 
 import { style } from "@vanilla-extract/css";
 
-import { vars } from "./theme.css";
+import { vars } from "@/shared/styles/theme.css.ts";
 
 // =========================
 // Title
@@ -27,14 +27,14 @@ export const largeTitleR32 = style({
   lineHeight: "134%",
 });
 
-export const title2R24 = style({
+export const title1 = style({
   fontFamily: vars.font.heading,
   fontSize: "24px",
   fontWeight: vars.fontWeight.regular,
   lineHeight: "134%",
 });
 
-export const title1R22 = style({
+export const title2 = style({
   fontFamily: vars.font.heading,
   fontSize: "22px",
   fontWeight: vars.fontWeight.regular,
@@ -45,21 +45,21 @@ export const title1R22 = style({
 // Large Body
 // =========================
 
-export const largeBody1R32 = style({
+export const largeBody1 = style({
   fontFamily: vars.font.body,
   fontSize: "32px",
   fontWeight: vars.fontWeight.regular,
   lineHeight: "134%",
 });
 
-export const largeBody2R18 = style({
+export const largeBody2 = style({
   fontFamily: vars.font.body,
   fontSize: "18px",
   fontWeight: vars.fontWeight.regular,
   lineHeight: "132%",
 });
 
-export const largeBody3R16 = style({
+export const largeBody3 = style({
   fontFamily: vars.font.body,
   fontSize: "16px",
   fontWeight: vars.fontWeight.regular,
@@ -70,63 +70,63 @@ export const largeBody3R16 = style({
 // Body
 // =========================
 
-export const body1B18 = style({
+export const body1 = style({
   fontFamily: vars.font.body,
   fontSize: "18px",
   fontWeight: vars.fontWeight.bold,
   lineHeight: "25.2px",
 });
 
-export const body2B16 = style({
+export const body2 = style({
   fontFamily: vars.font.body,
   fontSize: "16px",
   fontWeight: vars.fontWeight.bold,
   lineHeight: "134%",
 });
 
-export const body3M16 = style({
+export const body3 = style({
   fontFamily: vars.font.body,
   fontSize: "16px",
   fontWeight: vars.fontWeight.medium,
   lineHeight: "24px",
 });
 
-export const body4B14 = style({
+export const body4 = style({
   fontFamily: vars.font.body,
   fontSize: "14px",
   fontWeight: vars.fontWeight.bold,
   lineHeight: "20px",
 });
 
-export const body5SM14 = style({
+export const body5 = style({
   fontFamily: vars.font.body,
   fontSize: "14px",
   fontWeight: vars.fontWeight.semibold,
   lineHeight: "20px",
 });
 
-export const body6M14 = style({
+export const body6 = style({
   fontFamily: vars.font.body,
   fontSize: "14px",
   fontWeight: vars.fontWeight.medium,
   lineHeight: "136%",
 });
 
-export const body7B12 = style({
+export const body7 = style({
   fontFamily: vars.font.body,
   fontSize: "12px",
   fontWeight: vars.fontWeight.bold,
   lineHeight: "16px",
 });
 
-export const body8SM12 = style({
+export const body8 = style({
   fontFamily: vars.font.body,
   fontSize: "12px",
   fontWeight: vars.fontWeight.semibold,
   lineHeight: "20px",
 });
 
-export const body9M12 = style({
+export const body9 = style({
   fontFamily: vars.font.body,
   fontSize: "12px",
   fontWeight: vars.fontWeight.medium,
@@ -137,28 +137,28 @@ export const body9M12 = style({
 // Detail
 // =========================
 
-export const detail1M14 = style({
+export const detail1 = style({
   fontFamily: vars.font.body,
   fontSize: "14px",
   fontWeight: vars.fontWeight.medium,
   lineHeight: "20px",
 });
 
-export const detail2B12 = style({
+export const detail2 = style({
   fontFamily: vars.font.body,
   fontSize: "12px",
   fontWeight: vars.fontWeight.bold,
   lineHeight: "16px",
 });
 
-export const detail3B10 = style({
+export const detail3 = style({
   fontFamily: vars.font.body,
   fontSize: "10px",
   fontWeight: vars.fontWeight.bold,
   lineHeight: "14px",
 });
 
-export const detail4M10 = style({
+export const detail4 = style({
   fontFamily: vars.font.body,
   fontSize: "10px",
   fontWeight: vars.fontWeight.medium,
