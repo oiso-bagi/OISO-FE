d7d7f1bd2acdd9224b1604c462244db7c5657dba
jeniferkim <jeniferkim1673@naver.com>
2026-07-24 15:06:30 +0900

    fix: ISO 날짜-시간 응답 처리 방식 수정(#19)

--- src/pages/home/utils/homeFormat.ts
+++ src/pages/home/utils/homeFormat.ts
@@ -3,7 +3,9 @@
  * timezone 영향을 피하려고 Date 파싱 대신 문자열을 직접 나눕니다.
  */
 export const formatSavedDate = (isoDate: string): string => {
-  const [year, month, day] = isoDate.split("-");
+  const [year, month, day] = isoDate.split("T")[0].split("-");
+
+  if (!year || !month || !day) return "-";
 
   return `${year}. ${Number(month)}. ${Number(day)}.`;
 };
