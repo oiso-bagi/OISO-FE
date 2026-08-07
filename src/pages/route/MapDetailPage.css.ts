73cce4d694de0cdd67fb2e06126f7040e19c8c43
jeniferkim <jeniferkim1673@naver.com>
2026-08-07 13:16:37 +0900

    fix: 지도 상세 탭 스타일의 템플릿 리터럴 보간 오류 수정

--- src/pages/route/MapDetailPage.css.ts
+++ src/pages/route/MapDetailPage.css.ts
@@ -90,8 +90,8 @@ export const dayTab = style([
     color: vars.color.black,
     backgroundColor: vars.color.white,
 
-    border: "2px solid ${vars.color.black}",
-    boxShadow: "1.5px 2px 0 ${vars.color.black}",
+    border: `2px solid ${vars.color.black}`,
+    boxShadow: `1.5px 2px 0 ${vars.color.black}`,
 
     cursor: "pointer",
 
