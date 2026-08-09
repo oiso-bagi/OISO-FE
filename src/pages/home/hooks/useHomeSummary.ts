939816abbf92f6e289cc1484bde4750e0b32aff7
jeniferkim <jeniferkim1673@naver.com>
2026-08-09 23:35:58 +0900

    feat: 저장 루트 완료·삭제에 낙관적 업데이트 적용 (#39)

--- src/pages/home/hooks/useHomeSummary.ts
+++ src/pages/home/hooks/useHomeSummary.ts
@@ -3,11 +3,13 @@ import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "@/shared/query/queryKeys";
 
 import { getHomeSummary } from "../api/homeApi";
-import { USE_MOCK_HOME_DATA, mockHomeSummary } from "../mocks/homeMocks";
+import { USE_MOCK_HOME_DATA, getMockHomeSummary } from "../mocks/homeMocks";
 
 export const useHomeSummary = () => {
   return useQuery({
     queryKey: queryKeys.home.summary(),
-    queryFn: USE_MOCK_HOME_DATA ? async () => mockHomeSummary : getHomeSummary,
+    queryFn: USE_MOCK_HOME_DATA
+      ? async () => getMockHomeSummary()
+      : getHomeSummary,
   });
 };
